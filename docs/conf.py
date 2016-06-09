014de5cc1db886b0b502f031f2b41dd75b602b3c
Ted Johnson <tjohnson@osisoft.com>
2016-06-09 14:47:48 -0700

    custom stylesheet

--- docs/conf.py
+++ docs/conf.py
@@ -100,6 +100,9 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'default'
 
+def setup(app):
+    app.add_stylesheet('docs.css')
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
